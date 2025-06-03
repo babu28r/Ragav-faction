6a2ed41fdd474a7478653415a39a16dfdd06d4ef
Babu R <babu.r28062004@gmail.com>
2025-06-03 10:10:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,32 @@ function showNavbar(){
 function closeNavbar(){
   sidenav.style.left="-60%"
 }
+
+
+  function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+  }
+
+
+  const productName = getQueryParam('name');
+  const productPrice = getQueryParam('price');
+
+ 
+  if (productName && productPrice) {
+    document.getElementById('product-name').textContent = `Product: ${productName}`;
+    document.getElementById('product-price').textContent = `Price: ₹${productPrice}`;
+  }
+
+
+  function subscribe() {
+    const email = document.getElementById("newsletter-email").value;
+    if (email) {
+      alert("Subscribed with: " + email);
+     
+    } else {
+      alert("Please enter a valid email address.");
+    }
+  }
+
+
